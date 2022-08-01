8f0377483fd6b16601d2901c2e2e0cb74165ffcb
AfonsoBatista7 <afonsobatista13@gmail.com>
2022-08-01 13:23:08 +0100

    Merge duplicate documents

--- src/org/rage/pluginstats/mongoDB/DataBaseManager.java
+++ src/org/rage/pluginstats/mongoDB/DataBaseManager.java
@@ -241,6 +241,10 @@ public class DataBaseManager {
 		return mongoDB.getPlayerByName(name);
 	}
 	
+	public MongoCursor<Document> getAllPlayersByName(String playerName) {
+		return mongoDB.getAllPlayersByName(playerName);
+	}
+	
 	public Document getPlayer(UUID playerId) {
 		return mongoDB.getPlayer(playerId);
 	}
@@ -276,5 +280,7 @@ public class DataBaseManager {
 	public Document getDiscordUserByPlayer(UUID playerId) {
 		return getDiscordUser(getPlayer(playerId).getString("link"));
 	}
+	
+	
 	
 }
